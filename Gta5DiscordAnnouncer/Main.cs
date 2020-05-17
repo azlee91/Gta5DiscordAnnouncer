fbda112556e47c306672cd2dc7bb6eef4942c40d
Alvin Lee <algrn91@gmail.com>
2020-05-17 14:04:50 -0700

    Add project files.

---
+++ Gta5DiscordAnnouncer/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+
+namespace Gta5DiscordAnnouncer {
+    public class Main : BaseScript {
+        public Main() {
+            Console.WriteLine("Loading announcer script");
+            EventHandlers["esx:playerLoaded"] += new Action<string, dynamic>(HandleOnPlayerLoaded);
+        }
+
+        private void HandleOnPlayerLoaded(string playerId, dynamic esxPlayer) {
+            Console.WriteLine($"An esx player [{esxPlayer.getName()}] has loaded with ID: {esxPlayer.getIdentifier()}!");
+        }
+    }
+}
---
+++ Gta5DiscordAnnouncer/dist/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Manifest data
+fx_version 'bodacious'
+games {'gta5'}
+
+name 'Gta 5 Discord Announcer'
+author 'LordTargaryen'
+description 'Gta 5 Discord Announcer'
+version '1.0'
+
+server_script 'server/Gta5DiscordAnnouncer.net.dll'
